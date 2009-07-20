e2a29824e6354633d104e75436673fcbf555f425
Sven Herzberg <sven.herzberg@lanedo.com>
2009-07-20 13:56:01 +0000

    2009-01-08  Sven Herzberg  <sven@imendio.com>
    	Properly forward the depth information.
    
    	* engine/sapwood-pixmap.c,
    	* engine/sapwood-pixmap.h,
    	* engine/sapwood-style.c,
    	* engine/theme-pixbuf.c,
    	* engine/theme-pixbuf.h: properly forward the depth information into
    	the sapwood server
    
    
    
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk/sapwood@18897 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- engine/sapwood-pixmap.c
+++ engine/sapwood-pixmap.c
@@ -88,6 +88,7 @@ sapwood_pixmap_get_for_file (const char *filename,
                              int         border_right,
                              int         border_top,
                              int         border_bottom,
+                             int         depth,
                              GError    **err)
 {
   SapwoodPixmap     *self;
@@ -108,7 +109,7 @@ sapwood_pixmap_get_for_file (const char *filename,
 
   req->base.op       = PIXBUF_OP_OPEN;
   req->base.length   = sizeof(*req) + flen + 1;
-  req->depth         = 24;
+  req->depth         = depth;
   req->border_left   = border_left;
   req->border_right  = border_right;
   req->border_top    = border_top;
