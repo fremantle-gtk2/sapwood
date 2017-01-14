1d5dde99bc905268ed4844ea67b72384ae8834bd
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2017-01-15 01:22:08 +0200

    remove autofoo-1.8 references
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,10 @@
 
 set -x
 libtoolize --automake
-aclocal-1.8 || aclocal
+aclocal
 autoconf
 autoheader
-automake-1.8 --add-missing --foreign || automake --add-missing --foreign
+automake --add-missing --foreign
 
 if test x$NOCONFIGURE = x; then
   ./configure --enable-maintainer-mode --enable-debug "$@"
